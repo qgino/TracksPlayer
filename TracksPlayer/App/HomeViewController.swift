2df47812a6a48124d86014a468406055a6b21f9c
Gino Quaglietta <qugino@gmail.com>
2025-06-24 00:21:58 +0200

    restart project

---
+++ TracksPlayer/App/HomeViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class HomeViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+        // Do any additional setup after loading the view.
+    }
+}
